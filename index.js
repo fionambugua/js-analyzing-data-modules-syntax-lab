1eec520c176f617cd88f93606235c78536fb15d8
Fiona <fionambuguanjeri@gmail.com>
2026-07-07 15:37:12 +0300

    analyzing data modules

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+function combineUsers(...userArrays) {
+  const users = [].concat(...userArrays);
 
+  const today = new Date();
+  const merge_date = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
 
+  return {
+    merge_date,
+    users,
+  };
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
+};
+
+module.exports = {
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
